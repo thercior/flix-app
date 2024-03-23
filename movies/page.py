76c90c7613ff12b8c3bec9b402bdd8192fdf6acf
Thércio Rodrigues <thercior@gmail.com>
2024-03-23 11:55:57 -0300

    :recycle: refactor: Rearranjo página de Filmes e Reviews para exibir resultados da consulta a API

--- movies/page.py
+++ movies/page.py
@@ -1,29 +1,28 @@
 import pandas as pd
 import streamlit as st
-from st_aggrid import AgGrid
-
-
-movies = [
-    {
-        'id': 1,
-        'nome': 'Titanic'
-    },
-    {
-        'id': 2,
-        'nome': 'Bad Boys'
-    },
-    {
-        'id': 3,
-        'nome': 'Velozes e Furiosos'
-    },
-]
+from movies.services import MovieService
+from st_aggrid import AgGrid, ExcelExportMode
 
 
 def show_movies():
-    st.title('Lista de filmes')
+    movie_service = MovieService()
+    movies = movie_service.get_movies()
 
-    AgGrid(
-        data=pd.DataFrame(movies),
-        reload_data=True,
-        key='movies_grid'
-    )
+    if movies:
+        st.subheader('Lista de filmes')
+        # Transformando em um dataframe
+        movies_df = pd.json_normalize(movies)
+        # movies_df = movies_df.drop(columns=['atores', 'genero.id'])  # corrigir isso depois com a aula
+
+        AgGrid(
+            data=movies_df,
+            reload_data=True,
+            columns_auto_size_mode=True,
+            enableSorting=True,
+            enableFilter=True,
+            enableColResize=True,
+            excel_export_mode=ExcelExportMode.MANUAL,
+            key='movies_grid'
+        )
+    else:
+        st.warning('Nenhum filme foi encontrado')
--- review/page.py
+++ review/page.py
@@ -1,29 +1,27 @@
 import pandas as pd
 import streamlit as st
-from st_aggrid import AgGrid
-
-
-reviews = [
-    {
-        'id': 1,
-        'stars': 5
-    },
-    {
-        'id': 2,
-        'stars': 3
-    },
-    {
-        'id': 3,
-        'stars': 4
-    },
-]
+from review.service import ReviewService
+from st_aggrid import AgGrid, ExcelExportMode
 
 
 def show_reviews():
-    st.title('Lista de Avaliações dos filmes')
+    reviews_service = ReviewService()
+    reviews = reviews_service.get_reviews()
 
-    AgGrid(
-        data=pd.DataFrame(reviews),
-        reload_data=True,
-        key='reviews_grid'
-    )
+    if reviews:
+        st.subheader('Lista de Avaliações dos filmes')
+        # Montagem do Dataframe
+        reviews_df = pd.json_normalize(reviews)
+
+        AgGrid(
+            data=reviews_df,
+            reload_data=True,
+            columns_auto_size_mode=True,
+            enableSorting=True,
+            enableFilter=True,
+            enableColResize=True,
+            excel_export_mode=ExcelExportMode.MANUAL,
+            key='reviews_grid'
+        )
+    else:
+        st.warning('Nenhuma avaliação foi encontrada')
