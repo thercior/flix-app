5457bbaf8c4502be57b9497895e3df847d3dca9f
Thércio Rodrigues <thercior@gmail.com>
2024-04-05 22:08:57 -0300

    :lipstick: feat: alterando visualização: Gráfico e quantidade de filmes por gênero lado a lado

--- home/page.py
+++ home/page.py
@@ -9,15 +9,46 @@ def show_home():
 
     st.title('Estatísticas de Filmes')
 
-    if len(movie_stats['filmes_por_genero']) > 0:
-        st.subheader('Filmes por gênero')
-        fig = px.pie(
-            movie_stats['filmes_por_genero'],
-            values='count',
-            names='genero__nome',
-            title='Filmes por gênero'
-        )
-        st.plotly_chart(fig)
+    col1, col2 = st.columns(2, gap="large")
+
+    # Adicionando estilos as colunas
+    col1.markdown(
+        "<style>div[data-testid='stHorizontalBlock'] div[data-testid='stBlock'{flex-direction:column}</style>",
+        unsafe_allow_html=True
+    )
+    col2.markdown(
+        "<style>div[data-testid='stHorizontalBlock'] div[data-testid='stBlock'{flex-direction:column}</style>",
+        unsafe_allow_html=True
+    )
+
+    with col1:
+        if len(movie_stats['filmes_por_genero']) > 0:
+            st.subheader('Filmes por gênero')
+            fig = px.pie(
+                movie_stats['filmes_por_genero'],
+                values='count',
+                names='genero__nome',
+                title='Filmes por gênero',
+                height=400,
+            )
+            fig.update_layout(
+                legend=dict(
+                    orientation="v",
+                    yanchor="bottom",
+                    y=0.5,
+                    xanchor="left",
+                    x=0,
+                    font=dict(
+                        size=10,
+                    )
+                )
+            )
+            st.plotly_chart(fig)
+
+    with col2:
+        st.subheader('Quantidade de Filmes por gênero:')
+        for genre in movie_stats['filmes_por_genero']:
+            st.write(f"{genre['genero__nome']}: {genre['count']}")
 
     st.subheader('Total de Filmes cadastrados:')
     st.write(movie_stats['total_filmes'])
